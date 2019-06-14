ee97622e163405b364ae55b317d9d5d3b19e6903
igoist <493512132@qq.com>
2019-06-14 19:08:19 +0800

    hotfix HMR

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import { App } from './App';
 import './style.css';
 
 
 ReactDOM.render(
-  <App />,
+  <AppContainer>
+    <App />
+  </AppContainer>,
   document.getElementById('app')
 );
+
+if (module.hot) {
+  module.hot.accept();
+}
